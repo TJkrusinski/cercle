7d8cb495f28259e6a1f5001f491d1fe49c608a0d
TJ Krusinski <tjkrus@gmail.com>
2014-01-04 14:39:24 -0500

    subscriptions

---
+++ tests/subscribers.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var assert = require('chai').assert,
+	subs = require(__dirname+'/../lib/subscribers.js');
+
+var sub = {
+	
+};
+
+var sub2 = {
+	id: 'fooid fooid'
+};
+
+var id;
+
+describe('subscribers', function(){
+	describe('subs#subscribe()', function(){
+		it('subscribes a user to a topic', function(d){
+			subs.subscribe('footopic', sub, function(err, data){
+				assert.isFalse(err);
+				assert.isObject(data);
+				id = data.id;
+				d();
+			});
+		});
+	});
+
+	describe('subs#subscribe()', function(){
+		it('subscribes a user to a topic', function(d){
+			subs.subscribe('footopic', sub2, function(err, data){
+				assert.isFalse(err);
+				assert.isObject(data);
+				d();
+			});
+		});
+	});
+
+	describe('subs#get()', function(){
+		it('gets subs for a topic', function(d){
+			subs.get('footopic', function(err, data){
+				assert.isFalse(err);
+				assert.isArray(data);
+				assert.isObject(data[0]);
+				assert.ok(data.length > 0);
+				d();
+			});
+		});
+	});
+	
+	describe('subs#unsubscribe()', function(){
+		it('unsubscribe from a topic', function(d){
+			subs.unsubscribe('footopic', id, function(err, data){
+				assert.isFalse(err);
+				d();
+			});
+		});
+	});
+
+	describe('subs#get()', function(){
+		it('gets subs for a topic', function(d){
+			subs.get('footopic', function(err, data){
+				assert.isFalse(err);
+				assert.isObject(data[0]);
+				assert.isArray(data);
+				assert.ok(data.length > 0);
+				d();
+			});
+		});
+	});
+
+	describe('subs#unsubscribe()', function(){
+		it('unsubscribes all users', function(d){
+			subs.unsubscribe('footopic', function(err, data){
+				assert.isFalse(err);
+				d();
+			});
+		});
+	});
+
+	describe('subs#get()', function(){
+		it('gets subs for a topic', function(d){
+			subs.get('footopic', function(err, data){
+				assert.isFalse(err);
+				assert.isArray(data);
+				assert.ok(data.length == 0);
+				d();
+			});
+		});
+	});
+});
